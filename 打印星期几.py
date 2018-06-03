d09aefbfad1ad86c0800de7250d73bb1143405b3
FangfangXW <39868160+FangfangXW@users.noreply.github.com>
2018-06-03 09:45:42 +0800

    Add files via upload

---
+++ 打印星期几.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jun  3 09:44:24 2018
+
+@author: Administrator
+"""
+
+weeks=['星期一','星期二','星期三','星期四','星期五','星期六','星期天']
+for i in weeks:
+    if i=='星期六':
+        print('今天是：{}'.format(i))
+    else:
+        print(i)
